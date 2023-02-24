cc4a050b4d98b0c4e22cc4d034592b46fae5e92b
Nirob Barman <niroborin111@gmail.com>
2023-02-24 18:55:45 +0600

    uva updated

---
+++ Online-Judge-Home(UVa)/113 Power of Cryptography.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double n, p, k;
+    while (scanf("%lf %lf", &n, &p) != EOF)
+    {
+        k = pow(p, (1.0 / n));
+        printf("%.0lf\n", k);
+    }
+}
