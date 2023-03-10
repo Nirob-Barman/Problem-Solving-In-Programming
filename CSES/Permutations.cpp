7b1b543855cc37dd15b6cf65daad52fef3e0a09f
Nirob Barman <niroborin111@gmail.com>
2023-03-10 21:31:44 +0600

    Permutations

---
+++ CSES/Permutations.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n == 1)
+        printf("1\n");
+    else if(n<4)
+    {
+        printf("NO SOLUTION\n");
+    }
+    else if(n == 4)
+    {
+        printf("3 1 4 2\n");
+    }
+    else
+    {
+        for(int i=1;i<=n;i+=2)
+            printf("%d ",i);
+        for(int i=2;i<=n;i+=2)
+        {
+            printf("%d ",i);
+            if(i==n)
+                printf("\n");
+        }
+    }
+}
