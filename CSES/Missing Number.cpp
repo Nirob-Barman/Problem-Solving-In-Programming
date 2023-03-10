647cd8a1a1bf0fcc47e691b43736b34872976f73
Nirob Barman <niroborin111@gmail.com>
2023-03-10 19:38:05 +0600

    Repetitions

--- CSES/Missing Number.cpp
+++ CSES/Missing Number.cpp
@@ -6,12 +6,12 @@ int main()
 {
     long long n;
     scanf("%d",&n);
-    long long totalSum = (n*(n+1)/2);
+    long long arraySum = (n*(n+1)/2);
     int a[n-1],loopSum=0;
     for(int i=0;i<n-1;i++)
     {
         scanf("%d",&a[i]);
         loopSum+=a[i];
     }
-    printf("%d\n",totalSum - loopSum);
+    printf("%d\n",arraySum - loopSum);
 }
---
+++ CSES/Repetitions.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    int maxLength = 0, currentLength = 1;
+
+    for(int i=1;i<s.length();i++)
+    {
+        if(s[i]!=s[i-1])
+        {
+            maxLength = max(maxLength,currentLength);
+            currentLength = 0;
+        }
+        currentLength++;
+    }
+    maxLength = max(maxLength,currentLength);
+    cout<<maxLength<<endl;
+}
