3e5e92f15dd320f2198dc506f73af7d8f77dc64b
rsnirob <niroborin111@gmail.com>
2023-03-11 18:15:37 +0600

    updated

--- beecrowd/1001.c
+++ beecrowd/1001.c
@@ -3,13 +3,8 @@
 int main()
 {
     int A,B,X;
-
     scanf("%d\n%d",&A,&B);
-
     X=A+B;
-
     printf("X = %d\n",X);
-
     return 0;
-
 }
--- beecrowd/1003.c
+++ beecrowd/1003.c
@@ -3,12 +3,8 @@
 int main()
 {
     int A,B,SOMA;
-
     scanf("%d\n%d",&A,&B);
-
     SOMA=A+B;
-
     printf("SOMA = %d\n",SOMA);
-
     return 0;
 }
