16e8200b8e09cba4154c3034c3f3694efa841803
rsnirob <niroborin111@gmail.com>
2023-03-13 19:43:51 +0600

    11498 - Division of Nlogonia

---
+++ Online-Judge-Home(UVa)/11498 - Division of Nlogonia.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int k, n, m, x, y, i;
+    while (scanf("%d", &k) != EOF && k != 0)
+    {
+        scanf("%d %d", &n, &m);
+        for (i = 0; i < k; i++)
+        {
+            scanf("%d %d", &x, &y);
+            if (x == n || y == m)
+                printf("divisa\n");
+            else if (x > n && y > m)
+                printf("NE\n");
+            else if (x < n && y > m)
+                printf("NO\n");
+            else if (x < n && y < m)
+                printf("SO\n");
+            else if (x > n && y < m)
+                printf("SE\n");
+        }
+    }
+}
