53d76924103503670d51335136f3e74f87c34020
rsnirob <niroborin111@gmail.com>
2023-03-17 18:38:27 +0600

    10550 - Combination Lock

---
+++ Online-Judge-Home(UVa)/10550 - Combination Lock.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c, d, degree;
+    while (scanf("%d %d %d %d", &a, &b, &c, &d) != EOF)
+    {
+        if (!a && !b && !c && !d)
+            break;
+        degree = 1080 + ((a - b + 40) % 40 + (c - b + 40) % 40 + (c - d + 40) % 40) * 9;
+        printf("%d\n", degree);
+    }
+}
