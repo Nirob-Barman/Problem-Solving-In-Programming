44acca3364674da528c35fa838d60577caac4309
rsnirob <niroborin111@gmail.com>
2023-03-17 18:41:26 +0600

    01124 - Celebrity jeopardy

---
+++ Online-Judge-Home(UVa)/01124 - Celebrity jeopardy.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    char s[100];
+    while (scanf(" %[^\n]", s) != EOF)
+    {
+        // printf("%s",s);
+        puts(s);
+        // printf("\n");
+    }
+}
