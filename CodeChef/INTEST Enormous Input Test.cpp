6240c7ed27f2792f5b3b69892bb19516da7ed5b4
rsnirob <niroborin111@gmail.com>
2023-03-18 19:32:47 +0600

    INTEST Enormous Input Test

--- CodeChef/INTEST Enormous Input Test.cpp
+++ CodeChef/INTEST Enormous Input Test.cpp
@@ -1,16 +1,16 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 int main()
 {
-    int n,k;
-    scanf("%d %d",&n,&k);
-    int c=0;
-    for(int i=0;i<n;i++)
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int c = 0;
+    for (int i = 0; i < n; i++)
     {
         int x;
-        scanf("%d",&x);
-        if(x%k==0)
+        scanf("%d", &x);
+        if (x % k == 0)
             c++;
     }
-    printf("%d\n",c);
+    printf("%d\n", c);
 }
