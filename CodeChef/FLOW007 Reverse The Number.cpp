53d7db2eea8acdc12dd59948754ceff6aa3858d8
rsnirob <niroborin111@gmail.com>
2023-03-18 19:45:08 +0600

    #include <bits/stdc++.h>

---
+++ CodeChef/FLOW007 Reverse The Number.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        int n, reverseNumber = 0, r;
+        cin >> n;
+        for (; n > 0; n = n / 10)
+        {
+            r = n % 10;
+            reverseNumber = reverseNumber * 10 + r;
+        }
+        cout << reverseNumber << endl;
+    }
+    return 0;
+}
