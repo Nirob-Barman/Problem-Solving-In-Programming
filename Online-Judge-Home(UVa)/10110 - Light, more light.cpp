28afcd5dadae5ca1d0f6015dfae47563ee1eca78
rsnirob <niroborin111@gmail.com>
2023-03-18 08:30:23 +0600

    10110 - Light, more light

--- Online-Judge-Home(UVa)/10110 - Light, more light.cpp
+++ Online-Judge-Home(UVa)/10110 - Light, more light.cpp
@@ -1,13 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 int main()
 {
-    unsigned int n,s;
-    while(scanf("%u",&n)!=EOF && n!=0)
+    unsigned int n, s;
+    while (scanf("%u", &n) != EOF && n != 0)
     {
-        s=sqrt(n);
-        s=s*s;
-        if(s==n)
+        s = sqrt(n);
+        s = s * s;
+        if (s == n)
             printf("yes\n");
         else
             printf("no\n");
