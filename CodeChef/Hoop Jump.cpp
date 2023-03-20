d0965208f52db551374bc58e4362e80f1bd4a651
rsnirob <niroborin111@gmail.com>
2023-03-20 21:06:38 +0600

    Hoop Jump

---
+++ CodeChef/Hoop Jump.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", (n / 2) + 1);
+    }
+}
