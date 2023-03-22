a6403cfeb20bce77f758586505752d39086157b7
rsnirob <niroborin111@gmail.com>
2023-03-22 12:12:18 +0600

    11388 - GCD LCM

---
+++ Online-Judge-Home(UVa)/11388 - GCD LCM.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d %d",&a,&b);
+
+        if(b%a==0)
+            printf("%d %d\n",a,b);
+        else
+            printf("-1\n");
+    }
+}
