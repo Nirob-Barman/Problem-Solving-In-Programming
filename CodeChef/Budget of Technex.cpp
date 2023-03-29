416b3763b716ccd7670aa025a6b4a8c82e053875
rsnirob <niroborin111@gmail.com>
2023-03-29 22:44:11 +0600

    Budget of Technex

---
+++ CodeChef/Budget of Technex.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int r;
+        scanf("%d", &r);
+        printf("%d\n", ((r * 1000) / 2) / 5);
+    }
+}
